eec2c11460e12acf6b5600bf487e94bae04ea23d
hetbhalani <bhalanihet2006@gmail.com>
2025-02-16 00:29:29 +0530

    login page done

---
+++ frontend/src/pages/LogInForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+function LogInForm() {
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+
+  async function checkValidation(e, email, password){
+    e.preventDefault();
+
+    const response = await axios.post("http://localhost:3000/auth/login", {
+      email,password
+    });
+
+    console.log("login successful:", response.data);
+    setEmail("");
+    setPassword("");
+  }
+  
+  return (
+    <div >
+      <div className="card p-4 shadow rounded-4 shadow-lg" style={{ width: "350px" }}>
+        <h2 className="text-center mb-4">Log In</h2>
+        <form>
+          <div className="mb-3 ">
+            <label className="form-label text-start d-block">Email</label>
+            <input type="email" className="form-control" placeholder="Enter your email" value={email}
+            onChange={(e)=>{setEmail(e.target.value)}}/>
+          </div>
+
+          <div className="mb-3">
+            <label className="form-label text-start d-block">Password</label>
+            <input type="password" className="form-control" placeholder="Enter password" value={password}
+            onChange={(e)=>{setPassword(e.target.value)}}/>
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100 mt-3" onClick={(e) => checkValidation(e, email, password)}>
+            Log In
+          </button>
+
+          <p className="text-center mt-3">
+            Don't have an account? <Link to='/signup'>SignUp</Link> 
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LogInForm; 
