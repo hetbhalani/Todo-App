491b88b9a4d32ff417a0269349de9da11d2523a7
hetbhalani <bhalanihet2006@gmail.com>
2025-02-16 00:28:39 +0530

    idk

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const dotenv = require("dotenv")
 const cors = require('cors');
-const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
 
 const todoRoutes = require('./routes/todo');  
 const authRoutes = require('./routes/auth');
@@ -11,12 +12,19 @@ const authRoutes = require('./routes/auth');
 dotenv.config()
 
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true,
+}));
 app.use(cookieParser());
 
 
 app.use('/todo', todoRoutes);
 app.use('/auth', authRoutes);
+app.use((req, res, next) => {
+  console.log("Cookies received:", req.cookies);
+  next();
+});
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
